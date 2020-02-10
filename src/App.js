8f1b6c56c99c0d88b5043e8b99bf80becd814014
tomaspilgrim <tomaspilgrim@gmail.com>
2020-02-10 11:29:12 +0100

    Update 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wie dit leest... is gek!
         </a>
       </header>
     </div>
